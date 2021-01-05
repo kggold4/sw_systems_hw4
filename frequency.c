6567543b9808d04af52eed6db8110a46f57fdd2a
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-01-05 21:13:46 +0200

    update 2 frequency.c

--- frequency.c
+++ frequency.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #define NUM_LETTERS ((int)26)
+#define MAX_LINE 256
 
 // boolean enum
 typedef enum {FALSE=0, TRUE=1} boolean;
@@ -19,14 +20,28 @@ typedef struct node {
     struct node* children[NUM_LETTERS];
 } node;
 
+void lowerCase(char * string) {
+    for(int i = 0; i < strlen(string); i++) {
+        if(string[i] >= 'A' && string[i] <= 'Z') {
+            printf("found: %c", string[i]);
+            string[i] -= NUM_LETTERS;
+        }
+    }
+}
+
 void input() {
-    
+    char string[MAX_LINE];
+    fgets(string, strlen(string), stdin);
+    lowerCase(string);
+    printf("%s\n", string);
 }
 
 // main function
 int main() {
 
-    in = *boolean[NUM_LETTERS];
+    // *boolean[NUM_LETTERS] in;
+    input();
+
     
     return 0;
 }
