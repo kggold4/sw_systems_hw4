d42dca3bee7845b4873359d679da619b88d46157
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-01-05 20:17:52 +0200

    update 1 frequency.c

--- frequency.c
+++ frequency.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define NUM_LETTERS ((int)26)
 
+// boolean enum
+typedef enum {FALSE=0, TRUE=1} boolean;
+
+// struct of a node
+typedef struct node {
+
+    // each node save a leter
+    char letter;
+
+    // counter
+    long unsigned int count;
+
+    // pointer to children array
+    struct node* children[NUM_LETTERS];
+} node;
+
+void input() {
+    
+}
+
+// main function
 int main() {
+
+    in = *boolean[NUM_LETTERS];
     
     return 0;
 }
